694fef3709d8d066adb915423f32f5432db86219
Abhimanyu <abhimanyu@Abhimanyus-MacBook-Air.local>
2026-05-27 23:18:50 +0530

    second

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   transpilePackages: ['@dynasupz/types'],
   env: {
     API_URL: process.env.API_URL ?? 'http://localhost:4000/api/v1',
